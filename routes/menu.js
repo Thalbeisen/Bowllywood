337272fc0e8ecbe0fadc02182bed66317d233315
Lucile <sirius_86@outlook.fr>
2022-07-05 16:51:43 +0200

    Test connexion à mongoose

--- routes/menu.js
+++ routes/menu.js
@@ -9,4 +9,11 @@ router.post('/create', menuCtrl.createMeal);
 router.post('/update', menuCtrl.updateMeal);
 router.delete('/delete', menuCtrl.deleteMeal);
 
+// test mongoose & connxion to ddb.
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:3000/menu');
+const Cat = mongoose.model('Cat', { name: String });
+const kitty = new Cat({ name: 'Zildjian' });
+kitty.save().then(() => console.log('meow'));
+
 module.exports = router;
