a342c04005209de454f9a260932bd418104a406b
Lucile <sirius_86@outlook.fr>
2022-07-06 11:55:54 +0200

    Test model vers ddb

--- controllers/menu.js
+++ controllers/menu.js
@@ -3,17 +3,31 @@ var Menu = require('../models/menu');
 // get all menu
 exports.getAllMenu = (req, res) =>
 {
-    console.log(req);
+    // var fnCallback = function (err, docs) {console.log(err) console.log(docs) }
+    // var meals = Menu.find({}, fnCallback);
+    
+    var meals = await Menu.find({});
+    // console.log(meals)
 
     res.status(200).json(
     {
-        message: 'get all menu'
+        message: 'got the all menu !'
     });
 };
 
 // create a meal
 exports.createMeal = (req, res) =>
 {
+    var meal = new Menu({
+        name: 'Hahiti',
+        category: 1,
+        description: 'Loremm Ipsum',
+        ingredients: [2, 5, 12, 6, 22],
+        allergens: [3],
+        price: '15.2',
+        image: 'img_hahiti.png'
+    });
+
     res.status(200).json(
     {
         message: 'Création dun plat'
@@ -37,3 +51,18 @@ exports.deleteMeal = (req, res) =>
         message: 'Suppression dun plat'
     });
 };
+
+/*exports.testGetSomefields = (req, res) =>
+{
+    var fnCallback = function (err, docs) 
+    {
+        console.log(docs)
+        res.status(200).json(
+        {
+            message: 'get all menu'
+        });
+    }
+
+    var filters = {name: 'Hahiti'}
+    Menu.find(filters, 'name description' fnCallBack)
+}*/
