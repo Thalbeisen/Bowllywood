a66a371d2dd0898fd127348c8ecddd4dc8db407b
wilsonwesley <wesley-wilson@outlook.com>
2022-07-08 16:27:39 +0200

    Supress useless method and route

--- routes/prospects.js
+++ routes/prospects.js
@@ -9,7 +9,6 @@ router.post('/', prospectsCtrl.createProspectRequest);
 // id de la demande
 router.get('/:id', prospectsCtrl.getProspectRequestDetail);
 // id de l'utilisateur (route à modifier et mettre dans le router de user)
-// router.get('/:id', prospectsCtrl.getAllProspectRequestFromUser);
 router.get('/', prospectsCtrl.getAllProspectRequest);
 // id de la demande
 router.delete('/:id', prospectsCtrl.deleteProspectRequest);
