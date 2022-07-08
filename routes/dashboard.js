515b21a9e422f23d6d8f1ba45af3e096244afa0e
Thalbeisen <thibault.halbeisendev@gmail.com>
2022-07-08 14:30:00 +0200

    Linter fix

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-const dashboardController = require('../controllers/dashboard');
+// const dashboardController = require('../controllers/dashboard');
 
-router.get('/', dashboardController.userGreeting);
+// router.get('/', dashboardController.userGreeting);
 
-module.exports = router
+module.exports = router;
