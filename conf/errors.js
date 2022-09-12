97e140d4718106193e892aaecaa856f57e6f6bb4
Lucile <sirius_86@outlook.fr>
2022-09-12 15:16:20 +0200

    correction fichier erreurs

--- conf/errors.js
+++ conf/errors.js
@@ -1,43 +1,64 @@
+let entityDesc;
+let errorEntity;
+
 /**
  * Get the description/string of the current entity
  * @param  {string} entity 		The entity 'code'
  * @return {string}        		The entity description
  */
-getEntityDesc = function (entity) {
-	switch (entity) {
-		case 'USER':
-			entityDesc = 'de votre compte ';
-			break;
-		case 'PROSPECT':
-			entityDesc = 'du prospect ';
-			break;
-		case 'RESERV':
-			entityDesc = 'de la réservation ';
-			break;
-		case 'MENU':
-			entityDesc = 'du plat ';
-			break;
-		case 'ROLE':
-			entityDesc = 'du role ';
-			break;
-		case 'STOCK':
-			entityDesc = 'du produit ';
-			break;
-		default:
-			entityDesc = '';
-			break;
-	}
+const getEntityDesc = function (entity) {
+    switch (entity) {
+        case 'USER':
+            entityDesc = 'de votre compte ';
+            break;
+        case 'PROSPECT':
+            entityDesc = 'du prospect ';
+            break;
+        case 'RESERV':
+            entityDesc = 'de la réservation ';
+            break;
+        case 'MENU':
+            entityDesc = 'du plat ';
+            break;
+        case 'ROLE':
+            entityDesc = 'du role ';
+            break;
+        case 'STOCK':
+            entityDesc = 'du produit ';
+            break;
+        default:
+            entityDesc = '';
+            break;
+    }
 
-	return entityDesc;
+    return entityDesc;
+};
+
+
+/** Errors list * */
+errorsList {
+	createError = function (entity) {
+	    errorEntity = getEntityDesc(entity);
+
+	    return `La création ${errorEntity}a échoué. Veuillez réessayer plus tard ou contacter l'assistance tehnique si l'erreur persiste.`;
+	},
+
+	updateError : {
+	    'Une erreur est survenue lors de la modification. Veuillez réessayer plus tard.';
+	},
+
+	deleteError : {
+		'Une erreur est survenue lors de la tentative de suppression.';
+	},
+
+	emptyList : {
+		'Aucune données n\'a été trouvé.';
+	},
+
+	listError : {
+		"Impossible d'accéder à la liste demandée."
+	},
+
 }
 
-/** Errors list **/
-exports.updateError = 'Une erreur est survenue lors de la modification. Veuillez réessayer plus tard.';
-
-exports.createError = (entity) => {
-
-	errorEntity = getEntityDesc(entity);
-
-	return `La création ${errorEntity}a échoué. Veuillez réessayer plus tard ou contacter l'assistance tehnique si l'erreur persiste.`;
-}
-
+module.exports = erroList;
