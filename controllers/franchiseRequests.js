78372ab39e025984d26992d019cccc1c6521caa0
wilsonwesley <wesley-wilson@outlook.com>
2022-09-14 15:28:21 +0200

    Add getAll, getOne, archive and edit methods and routes

--- controllers/franchiseRequests.js
+++ controllers/franchiseRequests.js
@@ -1,10 +1,11 @@
 const FranchiseRequest = require('../models/franchiseRequests');
+const User = require('../models/users');
 const errors = require('../conf/errors');
 
 const entity = 'FRANCHISE_REQUEST';
 
 /**
- * Create a prospect request with or without authentification
+ * Create a franchise request.
  * @param {Request} req
  * @param {Response} res
  */
@@ -19,5 +20,100 @@ exports.addFranchiseRequest = async (req, res) => {
         res.status(201).json(subscriptionRequest);
     } catch (error) {
         res.status(400).json(errors.createError(entity));
+    }
+};
+
+/**
+ * Retrieve a specific franchise request informations
+ * @param {Request} req
+ * @param {Response} res
+ */
+exports.getFranchiseRequestDetail = async (req, res) => {
+    try {
+        const franchiseResquestDetail = await FranchiseRequest.findById(
+            req.params.id
+        ).exec();
+
+        if (!franchiseResquestDetail) {
+            res.status(404).json(errors.emptyList);
+        }
+
+        res.status(200).json(franchiseResquestDetail);
+    } catch (error) {
+        res.status(403).json(errors.listError);
+    }
+};
+
+/**
+ * Retrieve every franchise requests
+ * @param {Request} req
+ * @param {Response} res
+ */
+exports.getAllFranchiseRequests = async (req, res) => {
+    try {
+        const allFranchiseResquests = await FranchiseRequest.find({});
+        if (!allFranchiseResquests) {
+            res.status(404).json(errors.emptyList);
+        }
+        res.status(200).json(allFranchiseResquests);
+    } catch (error) {
+        res.status(403).json(errors.listError);
+    }
+};
+
+/**
+ * Archive a franchise request
+ * @param {Request} req
+ * @param {Response} res
+ */
+exports.archiveFranchiseRequest = async (req, res) => {
+    try {
+        const archiveRequest = await FranchiseRequest.findByIdAndUpdate(
+            req.params.id,
+            {
+                ...req.body,
+                deletedAt: Date.now(),
+            }
+        );
+        if (!archiveRequest) {
+            res.status(404).json(errors.emptyList);
+        }
+        res.status(200).json(archiveRequest);
+    } catch (error) {
+        res.status(403).json(errors.deleteError);
+    }
+};
+
+/**
+ * Edit a franchise request status
+ * @param {Request} req
+ * @param {Response} res
+ */
+exports.editFranchiseRequest = async (req, res) => {
+    try {
+        const editRequestStatus = await FranchiseRequest.findByIdAndUpdate(
+            req.params.id,
+            {
+                ...req.body,
+            },
+            { returnDocument: 'after' }
+        );
+
+        if (!editRequestStatus) {
+            res.status(404).json(errors.updateError(entity));
+        }
+
+        if (editRequestStatus.status === 'ACCEPTED') {
+            const user = await User.findOne({
+                _id: req.body.userID,
+            });
+            // eslint-disable-next-line no-underscore-dangle
+            user.franchiseContracts.push(editRequestStatus._id);
+            user.save();
+        }
+
+        res.status(200).json(editRequestStatus);
+    } catch (error) {
+        res.status(403).json(errors.updateError);
     }
 };
--- routes/franchiseRequests.js
+++ routes/franchiseRequests.js
@@ -2,9 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
-// Controllers
 const franchiseRequestsCtrl = require('../controllers/franchiseRequests');
 
 router.post('/', franchiseRequestsCtrl.addFranchiseRequest);
+router.get('/:id', franchiseRequestsCtrl.getFranchiseRequestDetail);
+router.get('/', franchiseRequestsCtrl.getAllFranchiseRequests);
+router.patch('/delete/:id', franchiseRequestsCtrl.archiveFranchiseRequest);
+router.patch('/edit/:id', franchiseRequestsCtrl.editFranchiseRequest);
 
 module.exports = router;
