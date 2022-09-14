24f2a9416bb468a1c70b40af7de0ed7c08b679ce
wilsonwesley <wesley-wilson@outlook.com>
2022-09-14 13:26:54 +0200

    Adjust add method

--- controllers/franchiseRequests.js
+++ controllers/franchiseRequests.js
@@ -12,14 +12,10 @@ exports.addFranchiseRequest = async (req, res) => {
     try {
         const fanchiseRequest = new FranchiseRequest({
             ...req.body,
+            user_id: req.body.userID,
         });
         const subscriptionRequest = await fanchiseRequest.save();
-        if (subscriptionRequest === null) {
-            res.status(404).json(errors.createError(entity));
-        }
-        // eslint-disable-next-line no-underscore-dangle
-        fanchiseRequest.user_id.push(req.body.userID);
-        fanchiseRequest.save();
+
         res.status(201).json(subscriptionRequest);
     } catch (error) {
         res.status(400).json(errors.createError(entity));
