092930131c900ff34886f8aec242a6dc1af8dd41
Thalbeisen <thibault.halbeisendev@gmail.com>
2022-09-16 12:08:07 +0200

    Corrections diverses

--- routes/users.js
+++ routes/users.js
@@ -14,8 +14,6 @@ router.post('/add', userController.userNew);
 
 router.get('/validate/:validationToken', userController.userValidate);
 
-// router.get('/:id/renewlink/:uniqueString', userController.validationTokenRenew);
-
 router.get('/:id', auth, userController.userDetails);
 
 router.patch('/:id', auth, userController.userEdit);
