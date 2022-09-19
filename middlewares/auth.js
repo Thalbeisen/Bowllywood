2e400de815b6eb2733e33a1c25a6bfaef643e8c8
Lucile <sirius_86@outlook.fr>
2022-09-19 11:28:30 +0200

    Récupération des merges

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -10,7 +10,7 @@ const jwt = require('jsonwebtoken');
 const auth = async (req, res, next) => {
     const authHeader = req.headers.authorization;
     const token = authHeader && authHeader.split(' ')[1];
-    
+
     try {
         const user = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
         if (!user) {
