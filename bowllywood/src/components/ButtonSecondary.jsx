a1e1447d84b55399b1e0f720906834a056f9a261
wilsonwesley <wesley-wilson@outlook.com>
2022-09-20 12:22:51 +0200

    Add ButtonSecondary component

---
+++ bowllywood/src/components/ButtonSecondary.jsx
@@ -0,0 +1,11 @@
+import './ButtonSecondary.scss';
+
+const ButtonSecondary = () => {
+    return (
+        <>
+            <button className='button-secondary'>Bouton secondaire</button>
+        </>
+    );
+}
+
+export default ButtonSecondary;
