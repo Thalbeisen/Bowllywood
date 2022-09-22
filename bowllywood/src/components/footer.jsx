37e5aab60875cec7f65777bbc81f900182255fa5
wilsonwesley <wesley-wilson@outlook.com>
2022-09-22 09:18:46 +0200

    Add footer

---
+++ bowllywood/src/components/footer.jsx
@@ -0,0 +1,12 @@
+import './../sass/styles.scss';
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+            <div className='col-12 footer'></div>
+        </>
+    );
+};
+
+export default Footer;
