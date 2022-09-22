00f89ba63c374e0bc66d77c8737d0d5ed25b8d29
Lucile <sirius_86@outlook.fr>
2022-09-22 13:53:53 +0200

    Ajustement formulaire, creation feuille scss & index.js dans screens

--- bowllywood/src/App.jsx
+++ bowllywood/src/App.jsx
@@ -4,6 +4,7 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './App.scss';
 import LoginScreen from './screens/login/';
 import RegisterScreen from './screens/register/';
+import MenuScreen from './screens/menu/';
 import Template from './components/Template';
 
 function App() {
@@ -14,8 +15,8 @@ function App() {
                       <Route path="/" element={<Template/>}>
                         {/* COMPLETER COMME L'EXEMPLE CI-DESSOUS */}
                         {/* <Route path="/test" element={<Test/>}/> */}
-                        <Route path="/reservation" />
-                        <Route path="/menu" />
+                        <Route path="/reservations" />
+                        <Route path="/menus" element={<MenuScreen />} />
                         <Route path="/mark" />
                         <Route path="/register" element={<RegisterScreen />} />
                         <Route path="/login" element={<LoginScreen />}/>
