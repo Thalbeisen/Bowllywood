b3771aafc1b3815d926204708d08a673d27a969c
wilsonwesley <wesley-wilson@outlook.com>
2022-09-23 04:47:41 +0200

    No change

--- bowllywood/src/components/HeaderTitle.jsx
+++ bowllywood/src/components/HeaderTitle.jsx
@@ -7,7 +7,7 @@ const Header = () => {
   const [title, setTitle] = useState('Titre h2');
 
   return (
-    <header className="d-flex flex-column align-items-center justify-content-center mb-5">
+    <header className="d-flex flex-column align-items-center justify-content-center">
       <img src={logo} className="App-logo" alt="logo" />
       <h2 className="m-0">{title}</h2>
   </header>
