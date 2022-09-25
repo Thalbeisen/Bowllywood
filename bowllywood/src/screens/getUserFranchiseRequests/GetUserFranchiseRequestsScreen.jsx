ebee1b674a294f917d8743e9a4af5adca55c3243
wilsonwesley <wesley-wilson@outlook.com>
2022-09-26 00:01:53 +0200

    Set up user franchise request feature with static user

--- bowllywood/src/screens/getUserFranchiseRequests/GetUserFranchiseRequestsScreen.jsx
+++ bowllywood/src/screens/getUserFranchiseRequests/GetUserFranchiseRequestsScreen.jsx
@@ -4,64 +4,97 @@ import { Col, Row, Container } from 'react-bootstrap';
 import Table from 'react-bootstrap/Table';
 
 const GetUserFranchiseRequestsScreen = () => {
+    const [userFranchiseRequests, setUserFranchiseRequests] = useState([]);
 
-  const [userFranchiseRequests, setUserFranchiseRequests] = useState([])
-  
-  useEffect(() => {
+    useEffect(() => {
+        getUserFranchiseRequests('6324d81ea70c53011eaf4733')
+            .then((res) => {
+                setUserFranchiseRequests(res.data.franchiseContracts);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
 
-    getUserFranchiseRequests('6324d81ea70c53011eaf4733')
-    .then((res) => {
-      setUserFranchiseRequests(res.data.franchiseContracts);
-
-      // setUserFranchiseRequests(res.data);
-      // console.log(res);
-      // console.log(res.data.franchiseContracts);
-      // console.log(userFranchiseRequests);
-    }).catch((err) => {
-      console.log(err);
-    })
-  }, [])
-
-
-
-
-
-  
     return (
         <>
             <Container>
                 <Row>
                     <Col>
-                    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>Montant estimé</th>
-          <th>Financement envisagé</th>
-          <th>Ville d'implantation</th>
-          <th>Status</th>
-          <th>Date de création</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-        userFranchiseRequests.map((franchiseContract, index) => {
-          console.log(franchiseContract);
-        <tr>
-          <td>{franchiseContract.estimatedAmount}</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-          <td></td>
-        </tr>
-        })
-        }
-      </tbody>
-    </Table>
+                        <Table striped bordered hover>
+                            <thead>
+                                <tr>
+                                    <th>Date de création</th>
+                                    <th>Montant estimé</th>
+                                    <th>Financement envisagé</th>
+                                    <th>Ville d'implantation</th>
+                                    <th>Status</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {userFranchiseRequests.map(
+                                    (franchiseContract, index) => {
+                                        return (
+                                            <tr>
+                                                <td
+                                                    key={
+                                                        'franchiseContract-' +
+                                                        { index }
+                                                    }
+                                                >
+                                                    {
+                                                        franchiseContract.createdAt
+                                                    }
+                                                </td>
+                                                <td
+                                                    key={
+                                                        'franchiseContract-' +
+                                                        { index }
+                                                    }
+                                                >
+                                                    {
+                                                        franchiseContract.estimatedAmount + ' €'
+                                                    }
+                                                </td>
+                                                <td
+                                                    key={
+                                                        'franchiseContract-' +
+                                                        { index }
+                                                    }
+                                                >
+                                                    {
+                                                        franchiseContract.hopedFinancing + ' €'
+                                                    }
+                                                </td>
+                                                <td
+                                                    key={
+                                                        'franchiseContract-' +
+                                                        { index }
+                                                    }
+                                                >
+                                                    {
+                                                        franchiseContract.shopLocation
+                                                    }
+                                                </td>
+                                                <td
+                                                    key={
+                                                        'franchiseContract-' +
+                                                        { index }
+                                                    }
+                                                >
+                                                    {franchiseContract.status}
+                                                </td>
+                                            </tr>
+                                        );
+                                    }
+                                )}
+                            </tbody>
+                        </Table>
                     </Col>
                 </Row>
             </Container>
         </>
-    )
-}
+    );
+};
 
 export default GetUserFranchiseRequestsScreen;
