52d8c4de09ff171d43b73c6addc303bb9638a0d6
wilsonwesley <wesley-wilson@outlook.com>
2022-09-25 23:22:22 +0200

    continue test

--- bowllywood/src/screens/getUserFranchiseRequests/GetUserFranchiseRequestsScreen.jsx
+++ bowllywood/src/screens/getUserFranchiseRequests/GetUserFranchiseRequestsScreen.jsx
@@ -8,11 +8,15 @@ const GetUserFranchiseRequestsScreen = () => {
   const [userFranchiseRequests, setUserFranchiseRequests] = useState([])
   
   useEffect(() => {
-    getUserFranchiseRequests('6324d81ea70c53011eaf4733').then((res) => {
-      setUserFranchiseRequests(res.data);
+
+    getUserFranchiseRequests('6324d81ea70c53011eaf4733')
+    .then((res) => {
+      setUserFranchiseRequests(res.data.franchiseContracts);
+
+      // setUserFranchiseRequests(res.data);
       // console.log(res);
-      // console.log(res.data);
-      console.log(userFranchiseRequests);
+      // console.log(res.data.franchiseContracts);
+      // console.log(userFranchiseRequests);
     }).catch((err) => {
       console.log(err);
     })
@@ -31,30 +35,26 @@ const GetUserFranchiseRequestsScreen = () => {
                     <Table striped bordered hover>
       <thead>
         <tr>
-          <th>#</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Username</th>
+          <th>Montant estimé</th>
+          <th>Financement envisagé</th>
+          <th>Ville d'implantation</th>
+          <th>Status</th>
+          <th>Date de création</th>
         </tr>
       </thead>
       <tbody>
+        {
+        userFranchiseRequests.map((franchiseContract, index) => {
+          console.log(franchiseContract);
         <tr>
-          <td>1</td>
+          <td>{franchiseContract.estimatedAmount}</td>
           <td>Mark</td>
           <td>Otto</td>
           <td>@mdo</td>
+          <td></td>
         </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td colSpan={2}>Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
+        })
+        }
       </tbody>
     </Table>
                     </Col>
