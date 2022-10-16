f54ea2f426ea91f1f8cd19b6c2bbe1efe0c20cff
wilsonwesley <wesley-wilson@outlook.com>
2022-10-16 15:23:38 +0200

    Wip new sidebar

--- bowllywood/src/components/Template.jsx
+++ bowllywood/src/components/Template.jsx
@@ -4,11 +4,67 @@ import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
 // IMPORTER LES ELEMENTS A METTRE DANS LA NAVIGATION COMME CI DESSOUS
 // import Test from './Test';
+import { render } from 'react-dom';
+import { Sidebar, SidebarItem } from 'react-responsive-sidebar';
+
+const items = [
+    <SidebarItem>
+        <Link to="/" className='text-decoration-none text-black text-center'>
+            <i className="fa-solid fa-house text-black flex-center"></i>
+            <p className='text-black'>Accueil</p>
+        </Link>
+    </SidebarItem>,
+    <SidebarItem>
+        <Link to="/reservations" className='text-decoration-none text-black text-center'>
+            <i className="fa-solid fa-money-bill text-black flex-center"></i>
+            <p className='text-black'>Réservation</p>
+        </Link>
+    </SidebarItem>,
+    <SidebarItem>
+        <Link to="/menus" className='text-decoration-none text-black text-center'>
+            <i className="fa-solid fa-utensils text-black flex-center"></i>
+            <p>Menu</p>
+        </Link>
+    </SidebarItem>,
+    <SidebarItem>
+        <Link to="/mark" className='text-decoration-none text-black text-center'>
+            <i className="fa-solid fa-star text-black flex-center"></i>
+            <p>Évaluation</p>
+        </Link>
+    </SidebarItem>,
+    <SidebarItem> 
+        <Link to="/franchise-request" className='text-decoration-none text-black text-center'>
+            <i className="fa-solid fa-people-roof text-black flex-center"></i>
+            <p>Devenir franchisé</p>
+        </Link>
+    </SidebarItem>,
+    <SidebarItem className="text-center">
+        <Link to="/my-franchise-requests" className='text-decoration-none text-black text-center'>
+            <i className="fa-solid fa-people-roof text-black flex-center"></i>
+            <p>Mes demandes de franchise</p>
+        </Link>
+    </SidebarItem>,
+    <SidebarItem>
+        <Link to="/register" className="text-black text-decoration-none flex-center">Inscription</Link>
+    </SidebarItem>,
+    <SidebarItem>
+        <Link to="/login" className="text-black text-decoration-none flex-center">Connexion</Link>
+    </SidebarItem>,
+    <SidebarItem>CCCCCCC</SidebarItem>,
+    <SidebarItem>AAAAAAAAAA</SidebarItem>,
+    <SidebarItem>BBBBBBBBBB</SidebarItem>,
+    <SidebarItem>CCCCCCC</SidebarItem>,
+    <SidebarItem>AAAAAAAAAA</SidebarItem>,
+    <SidebarItem>BBBBBBBBBB</SidebarItem>,
+    <SidebarItem>CCCCCCC</SidebarItem>,
+  ];
 
 const Template = () => {
     return (
         <>
-            <nav className="side-nav navbar-expand-lg">
+         <Sidebar content={items} background="#91D5A3" width={200} >
+          </Sidebar>
+            {/* <nav className="side-nav navbar-expand-lg">
                 <button class="navbar-toggler grow" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                 <span><i class="fa-solid fa-bars p-3"></i></span>
                 </button>
@@ -57,7 +113,7 @@ const Template = () => {
                     </div>
                 </div>
             </nav>
-            
+             */}
             <div className='main-content'>
                 {/* INSÉRER LES ÉLÉMENTS CENTRAUX ICI */}
                 <Outlet />
