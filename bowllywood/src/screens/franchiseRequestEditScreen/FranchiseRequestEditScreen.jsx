da87bcbc2274fe467823d4717798ff08eb24bb6c
wilsonwesley <wesley-wilson@outlook.com>
2022-10-20 09:16:45 +0200

    Wip edit franchise request

---
+++ bowllywood/src/screens/franchiseRequestEditScreen/FranchiseRequestEditScreen.jsx
@@ -0,0 +1,254 @@
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import { editFranchiseRequest } from '../../services/franchiseRequest';
+import HeaderTitle from '../../components/HeaderTitle';
+import InputText from '../../components/Input';
+import Button from '../../components/Button';
+import { Col, Row, Container } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getFranchiseRequestDetail } from '../../services/franchiseRequest';
+
+
+import './../../sass/styles.scss';
+
+const validationSchema = yup.object({
+    phone: yup.string().required('Ce champ est obligatoire'),
+    city: yup.string().required('Ce champ est obligatoire'),
+    estimatedAmount: yup.number().required('Ce champ est obligatoire'),
+    hopedFinancing: yup.number().required('Ce champ est obligatoire'),
+    shopLocation: yup.string().required('Ce champ est obligatoire'),
+    foodServiceExperience: yup.number().required('Ce champ est obligatoire'),
+    conditionOfUse: yup.bool().oneOf([true], 'Veuillez accepter les CGU').required(),
+    user_id: yup.string(),
+});
+
+const EditFranchiseRequestScreen = () => {
+
+    const [franchiseRequestDetail, setFranchiseRequestDetail] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    let { id } = useParams();
+
+    useEffect(() => {
+        getFranchiseRequestDetail(id)
+        .then((res) => {
+            setFranchiseRequestDetail(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    }, [id]);
+    
+    // EDITION
+    
+    const onSubmit = (values) => {
+        console.log(values);
+        editFranchiseRequest(values)
+            .then(() => {
+                alert('Modification de franchise effectuée');
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    };
+
+    const {values, errors, handleSubmit, handleChange, handleBlur, touched} =
+        useFormik({
+            initialValues: {
+                phone: '',
+                city: ``,
+                estimatedAmount: '',
+                hopedFinancing: '',
+                shopLocation: '',
+                foodServiceExperience: '',
+                conditionOfUse: true,
+                status: 'PENDING',
+                // DELETE LINE BELOW ONCE LOGIN FEATURE READY
+                user_id: '6324d81ea70c53011eaf4733',
+            },
+            validationSchema,
+            onSubmit,
+        });
+
+    return (
+        <>
+       { console.log(errors)}
+            <Container>
+                {/* <Row>
+                    <Col className='col-12 flex-center'>
+                        <img
+                            src="Bowllywood.png"
+                            alt="Logo du restaurant de bowls nommé Bowllywood"
+                        />
+                    </Col>
+                </Row> */}
+                <HeaderTitle />    
+                <Row>
+                    <Col>
+                        <form onSubmit={handleSubmit}>
+                            <Row className='justify-content-center gap-4'>
+                                <Col className="col-12 col-md-4 flex-center">
+                                    <InputText
+                                        name="phone"
+                                        desc="Numéro de téléphone *"
+                                        type="text"
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        value={values.phone}
+                                        placeholder="Ex: 0612345678"
+                                        error={
+                                            errors.phone &&
+                                            touched.phone &&
+                                            errors.phone
+                                        }
+                                    />
+                                </Col>
+                                <Col className="col-12 col-md-4 flex-center">
+                                    <InputText
+                                        name="city"
+                                        desc="Ville *"
+                                        type="text"
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        value={values.city}
+                                        placeholder="Ex: Paris"
+                                        error={
+                                            errors.city && touched.city && errors.city
+                                        }
+                                    />
+                                </Col>
+                            </Row>
+
+                            <Row className='justify-content-center gap-4'>
+                                <Col className="col-12 col-md-4 flex-center">
+                                    <InputText
+                                        name="estimatedAmount"
+                                        desc="Montant estimé de l'investissement *"
+                                        type="number"
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        value={values.estimatedAmount}
+                                        placeholder="Ex: 400000"
+                                        error={
+                                            errors.estimatedAmount &&
+                                            touched.estimatedAmount &&
+                                            errors.estimatedAmount
+                                        }
+                                    />
+                                </Col>
+                                <Col className="col-12 col-md-4 flex-center">
+                                    <InputText
+                                        name="hopedFinancing"
+                                        desc="Financement envisagé *"
+                                        type="number"
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        value={values.hopedFinancing}
+                                        placeholder="Ex: 200000"
+                                        error={
+                                            errors.hopedFinancing &&
+                                            touched.hopedFinancing &&
+                                            errors.hopedFinancing
+                                        }
+                                    />
+                                </Col>
+                            </Row>
+                            <Row className='justify-content-center gap-4'>
+                                <Col className="col-12 col-md-4 flex-center">
+                                    <InputText
+                                        name="shopLocation"
+                                        desc="Ville d'implantation *"
+                                        type="text"
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        value={values.shopLocation}
+                                        placeholder="Ex: Lyon"
+                                        error={
+                                            errors.shopLocation &&
+                                            touched.shopLocation &&
+                                            errors.shopLocation
+                                        }
+                                    />
+                                </Col>
+                                <Col className="col-12 col-md-4 flex-center">
+                                    <InputText
+                                        name="foodServiceExperience"
+                                        desc="Expérience dans la restauration *"
+                                        type="number"
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        value={values.foodServiceExperience}
+                                        placeholder="Ex: 3"
+                                        error={
+                                            errors.foodServiceExperience &&
+                                            touched.foodServiceExperience &&
+                                            errors.foodServiceExperience
+                                        }
+                                    />
+                                </Col>
+                            </Row>
+                            <Row className='justify-content-center gap-4'>
+                                <Col className="col-6 col-md-4 flex-center">
+                                    <InputText
+                                        name="conditionOfUse"
+                                        desc="J'ai lu et j'accepte les conditions générales d'utilisation *"
+                                        type="checkbox"
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        value={values.conditionOfUse}
+                                        error={
+                                            errors.conditionOfUse &&
+                                            touched.conditionOfUse &&
+                                            errors.conditionOfUse
+                                        }
+                                        checked
+                                    />
+                                </Col>
+                                <Col className='col-6 col-md-4 flex-center mb-5'>
+                                    <Button type="submit">Modifier ma demande</Button>
+                                </Col>
+                                <Col className="col-6 col-md-4 flex-center d-none">
+                                    <InputText
+                                        name="status"
+                                        desc=""
+                                        type="hidden"
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        value={values.status}
+                                        error={
+                                            errors.status &&
+                                            touched.status &&
+                                            errors.status
+                                        }
+                                    />
+                                </Col>
+                                <Col className="col-12 col-md-4 flex-center mb-5 d-none">
+                                    <InputText
+                                        name="user_id"
+                                        desc=""
+                                        type="hidden"
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        value={values.user_id}
+                                        error={
+                                            errors.user_id &&
+                                            touched.user_id &&
+                                            errors.user_id
+                                        }
+                                    />
+                                </Col>
+                            </Row>
+                                
+                               
+                        </form>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    );
+};
+export default EditFranchiseRequestScreen;
--- bowllywood/src/services/franchiseRequest.js
+++ bowllywood/src/services/franchiseRequest.js
@@ -3,3 +3,11 @@ import { AxiosInstance } from "../providers/axiosInstance";
 export const addFranchiseRequest = (values) => {
 	return AxiosInstance.post('/franchiseRequests/add', values);
 }
+
+export const getFranchiseRequestDetail = (id) => {
+    return AxiosInstance.get(`/franchiseRequests/${id}`);
+}
+
+export const editFranchiseRequest = (values) => {
+	return AxiosInstance.patch('/franchiseRequests/edit/:id', values);
+}
