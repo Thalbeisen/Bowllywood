45993d30085be6d8253aa9a5a85577514cd6af58
Lucile <sirius_86@outlook.fr>
2023-04-27 18:15:00 +0200

    test

--- bowllywood/src/providers/axiosInstance.js
+++ bowllywood/src/providers/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const AxiosInstance = axios.create({
-    baseURL: "https://bowllywood-8llo.onrender.com/"
-    // baseURL: 'http://localhost:5000'
+    // baseURL: "https://bowllywood-8llo.onrender.com/"
+    baseURL: 'http://localhost:5000'
 });
 
 AxiosInstance.interceptors.request.use(function(config) {
